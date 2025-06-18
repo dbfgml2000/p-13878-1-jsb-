d5de229a70be1ed095ff770b1b7b176f94e8b6fb
dbfgml20 <dbfgml20@gmail.com>
2025-06-18 12:47:07 +0900

    루트 url 사용, 서비스 활용

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,15 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @GetMapping("/")
-    @ResponseBody
-    public String index() {
-        return "SBB";
+    public String root() {
+        return "redirect:/question/list";
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/sbb")
     @ResponseBody
-    public String hello() {
-        return "Hello!";
+    public String index() {
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
 
 }
